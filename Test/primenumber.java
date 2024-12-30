a11f0df91d7b18500dc45d7345b1d9cadb5dc027
unknown <akkinapallynaveen1234@gmail.com>
2024-12-30 12:50:20 +0530

    first commit

---
+++ Test/primenumber.java
@@ -0,0 +1,23 @@
+package Test;
+
+public class primenumber {
+
+	public static void main(String[] args) {
+		int [][] mat={
+				{1,21,23},
+				{4,5,6},
+				{6,7,9},
+		};
+		for (int rows=0;rows<3;rows++){
+			for(int cols=0;cols<mat[rows].length;cols++) {
+				System.out.print(mat[rows][cols]+"");
+			}
+		}
+				
+	
+			{
+				System.out.println();
+			}
+	}
+
+}
---
+++ module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module oppsdemodecc24 {
+}
